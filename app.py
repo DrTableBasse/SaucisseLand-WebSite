964d6cd04e11f23eb6b40e08c51b8d393e3283ae
Samuel <nuez.samuel@gmail.com>
2025-06-18 16:13:15 +0200

    add default 404 error page

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from src.saucisseland import create_app
 from src.saucisseland.routes import api  # Adjust the import path as needed
-
+from flask import render_template
 app = create_app()
 
 
 app.register_blueprint(api)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/saucisseland/routes.py
+++ src/saucisseland/routes.py
@@ -77,7 +77,7 @@ def get_db_connection():
 
 @main.route("/nouvel-article", methods=["GET", "POST"])
 def nouvel_article():
-    """Affiche un formulaire de création d'article et enregistre un nouvel article"""
+    """Affiche un formulaire de création d'article et enregistre un nouvel article."""
     if request.method == "POST":
         titre = request.form["title"]
         contenu = request.form["content"]
@@ -122,6 +122,6 @@ def afficher_article(article_id):
     conn.close()
 
     if article is None:
-        return "Article non trouvé", 404
+        return render_template("404.html"), 404
 
     return render_template("article.html", article=article)
